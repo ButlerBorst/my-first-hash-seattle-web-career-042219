686d057adc1ab2f26ba12f31d4938512ba9b47a4
Alex Borst <butler.borst@gmail.com>
2019-04-16 21:03:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -30,12 +30,12 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
-
+shipping_manifest["gun powder"] = 4
+shipping_manifest
 
   # return the shipping_manifest hash below
 
